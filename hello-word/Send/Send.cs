2da8405c5a17711b71e78968b00d996e30194fac
Luc Oliveira <lucas.oliveira.almeida.ti@gmail.com>
2018-03-26 22:05:21 -0300

    tuto; first guide simple send / receiver

---
+++ hello-word/Send/Send.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+class Send
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+
+        var infos = Console.ReadLine();
+        while (infos != "exit")
+        {
+            SendMessage(factory, infos);
+            Console.WriteLine(" Write [exit] to exit.");
+            infos = Console.ReadLine();
+        }
+
+        //Console.ReadLine();
+    }
+
+    public static void SendMessage(ConnectionFactory factory, string message)
+    {
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            //string message = "Hello My Friendo!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "",
+                                 routingKey: "hello",
+                                 basicProperties: null,
+                                 body: body);
+            Console.WriteLine(" [x] Sent {0}", message);
+        }
+    }
+}
