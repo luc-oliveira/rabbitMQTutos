e179715d205da8c882366c92d481317999ad3f7b
Luc Oliveira <lucas.oliveira.almeida.ti@gmail.com>
2018-03-27 22:19:36 -0300

    tutos; second guide work queue
    Simple sender of tasks and workers to execute
    sent tasks, queue give the task to disponible
    workers, in this example, is just print a
    message on console

---
+++ work-queue/Worker/Worker.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Threading;
+
+class Worker
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "task_queu",
+                                 // durable indica que as mensagens devem ser guardadas
+                                 // dessa forma, mesmo que o rabbit restarte, a queue não perde a mensagens
+                                 // não é 100% funcional - de acordo com link deles - https://www.rabbitmq.com/tutorials/tutorial-two-dotnet.html
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            //garante que o rabbit não vai dar mais que uma mensagem pro worker por vez
+            channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+            Console.WriteLine(" [*] Waiting for messages.");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Received {0}", message);
+
+                int dots = message.Split('.').Length - 1;
+                Thread.Sleep(dots * 1000);
+
+                Console.WriteLine(" [x] Done");
+
+                //message acknowledgment - quando o worker acaba caindo e não executa a task,
+                // ou não responde pra fila se a task foi executada, temos o message acknowlegdment
+                // onde a fila repassa a mensagem pra outro worker
+                channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+            };
+            channel.BasicConsume(queue: "task_queu",
+                                 autoAck: false,
+                                 consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
